08d2f52af7f7e8bbd63f3fbd1729bbeb79ba194a
Maciej Malewicz <s18736@pjwstk.edu.pl>
2020-03-06 09:30:26 +0100

    init

---
+++ Tutorial1Solution/Tutorial1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial1
+{
+    
+    public class Class1
+    {
+        public int Attr { get; set; }
+
+        public void doThing()
+        {
+            
+        }
+    }
+}
